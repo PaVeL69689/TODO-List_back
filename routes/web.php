f7bce68e5caa30cf6b64e681bc260401c04bfa7d
pavel <studiovt@mail.ru>
2026-02-02 20:13:57 +0300

    собрал базовую структуру и создал миграции

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::controller(IndexController::class)->group(function (){
+    Route::get('/tasks', 'getTasks');
+    Route::get('/tasks/{id}', 'getTask');
+
+    Route::post('/tasks', 'createTask');
+    Route::put('/tasks/{id}', 'updateTask');
+    Route::delete('/tasks/{id}', 'deleteTask');
 });
